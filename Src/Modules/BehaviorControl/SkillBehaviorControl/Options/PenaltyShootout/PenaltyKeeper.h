d0caab18a2b858b4c93eb7ac8b5afa4910f53a64
Juan Diego Gómez <128845389+juangomezru@users.noreply.github.com>
2024-06-07 12:59:48 -0500

    Update PenaltyKeeper.h

--- Src/Modules/BehaviorControl/SkillBehaviorControl/Options/PenaltyShootout/PenaltyKeeper.h
+++ Src/Modules/BehaviorControl/SkillBehaviorControl/Options/PenaltyShootout/PenaltyKeeper.h
@@ -3,12 +3,30 @@ option(PenaltyKeeper)
   // Martin Kroker, 14.04.2013?
   auto penaltyKeeperShouldCatchBall = [&]
   {
+    const float offsetToYAxis = std::max(0.f, (theRobotPose.inverse() * Vector2f(theFieldDimensions.xPosOwnGoalArea, 0.f)).x());
+
+    const Vector2f& ballPositionRel = theBallModel.estimate.position;
     const Vector2f& ballVelocityRel = theBallModel.estimate.velocity;
-    // Return true if the ball is moving.
-    return ballVelocityRel.x() != 0.f;
+    // Return false if the ball does not move or moves away from the robot.
+    if(ballVelocityRel.x() >= 0.f)
+      return false;
+    // Return false if the ball has already intersected the line.
+    if((ballPositionRel.x() < 0.f && offsetToYAxis < ballPositionRel.x()) ||
+       (ballPositionRel.x() > 0.f && offsetToYAxis > ballPositionRel.x()))
+      return false;
+
+    Vector2f ballToIntersectPositionVector = theFieldBall.intersectionPositionWithOwnYAxis - ballPositionRel;
+    const float vectorLengthFactor = (ballPositionRel.x() - offsetToYAxis) / ballToIntersectPositionVector.x();
+    ballToIntersectPositionVector *= vectorLengthFactor;
+    const float s = ballToIntersectPositionVector.norm();
+    const float a = theBallSpecification.friction;
+    ASSERT(a < 0.f);
+    const float timeToIntersectYAxis = BallPhysics::timeForDistance(ballVelocityRel, s, a);
+
+    return between<float>(timeToIntersectYAxis, 0.01f, 10.5f) && theFieldBall.ballWasSeen(500) && (theGameState.isPenaltyShootout() || theFieldBall.isRollingTowardsOwnGoal);
   };
 
-initial_state(initial)
+  initial_state(initial)
   {
     transition
     {
@@ -18,6 +36,7 @@ initial_state(initial)
     action
     {
       theLookForwardSkill();
+      theDiveSkill({.request = MotionRequest::Dive::prepare});
     }
   }
 
@@ -25,9 +44,11 @@ initial_state(initial)
   {
     action
     {
-        theInterceptBallSkill({.interceptionMethods = bit(Interception::jumpRight),
-                                  .allowGetUp = !theGameState.isPenaltyShootout(),
-                                  .allowDive = theBehaviorParameters.keeperJumpingOn});
+      // Don't add other interception methods here until InterceptBall can handle them.
+      const unsigned interceptionMethods = bit(Interception::jumpLeft) | bit(Interception::jumpRight);
+      theInterceptBallSkill({.interceptionMethods = interceptionMethods,
+                             .allowGetUp = !theGameState.isPenaltyShootout(),
+                             .allowDive = theBehaviorParameters.keeperJumpingOn});
     }
   }
 }
