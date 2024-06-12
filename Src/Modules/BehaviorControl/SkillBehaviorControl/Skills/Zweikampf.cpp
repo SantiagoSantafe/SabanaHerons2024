edabb8a546d640853551b567f588eca0c5e3e6ce
joseph8884 <josegzm08@gmail.com>
2024-06-12 12:12:32 -0500

    implement new kick in pass to teammate

--- Src/Modules/BehaviorControl/SkillBehaviorControl/Skills/Zweikampf.cpp
+++ Src/Modules/BehaviorControl/SkillBehaviorControl/Skills/Zweikampf.cpp
@@ -267,7 +267,9 @@ class ZweikampfImpl : public ZweikampfImplBase
       {
         if(((kick == KickInfo::walkForwardsLeft ||
              kick == KickInfo::walkTurnLeftFootToRight ||
-             kick == KickInfo::walkTurnLeftFootToRightShifted) && theKickInfo[kick].range.min <= minForwardTurnRange) ||
+             kick == KickInfo::walkTurnLeftFootToRightShifted ||
+             kick == KickInfo::diagonalFastRight ||
+             kick == KickInfo::diagonalFastLeft) && theKickInfo[kick].range.min <= minForwardTurnRange) ||
            theKickInfo[kick].kickLeg != Legs::right) // skip clones
           continue;
         distances.push_back(theKickInfo[kick].range.min);
